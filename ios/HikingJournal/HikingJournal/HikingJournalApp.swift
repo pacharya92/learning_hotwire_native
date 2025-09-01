eabe955231c306f69216b96573adcc5e5e9ad7e3
Prakash Acharya <prakashacharya1992@gmail.com>
2025-09-01 13:34:06 -0400

    Add basic swift project

---
+++ ios/HikingJournal/HikingJournal/HikingJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingJournalApp.swift
+//  HikingJournal
+//
+//  Created by Prakash Acharya on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
