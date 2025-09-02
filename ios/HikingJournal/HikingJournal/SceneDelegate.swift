689ac5e4c6122d2cf6bcb6c3e5c3dcac8c1bd57a
Prakash Acharya <prakashacharya1992@gmail.com>
2025-09-01 20:23:40 -0400

    Add SceneDelegate as entry point

---
+++ ios/HikingJournal/HikingJournal/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  HikingJournal
+//
+//  Created by Prakash Acharya on 9/1/25.
+//
+
+import HotwireNative
+import UIKit
+
+let baseURL = URL(filePath: "http://localhost:3000")!
+
+class SceneDelegate {
+    
+}
